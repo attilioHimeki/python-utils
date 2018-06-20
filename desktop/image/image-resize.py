51bacf61c95166ae25083cf16175badf50697ab7
DESKTOP-CVQ6K9K\PC-LISBON <carotenuto.attilio@gmail.com>
2018-06-20 16:35:17 +0100

    optional disable AA

--- desktop/image/image-resize.py
+++ desktop/image/image-resize.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os, sys, argparse
 
-def resize(path, w, h, q=90, f='JPEG'):
+def resize(path, w, h, q=90, f='JPEG', sampleFilter=Image.ANTIALIAS):
 
     dirs = os.listdir(path)
 
@@ -12,7 +12,7 @@ def resize(path, w, h, q=90, f='JPEG'):
                 print("Resizing " + fullImagePath)
                 image = Image.open(fullImagePath)
                 name, ext = os.path.splitext(fullImagePath)
-                imResize = image.resize((w, h), Image.ANTIALIAS)
+                imResize = image.resize((w, h), sampleFilter)
                 imResize.save(name + '_resized.' + f, f, quality=q)
             else:
                 print("Skipping " + fullImagePath)
@@ -23,20 +23,17 @@ def isImageFile(path):
     return lowerPath.endswith(".jpg") or lowerPath.endswith(".png") or lowerPath.endswith(".gif") or lowerPath.endswith(".jpeg")
 
 
-def resizeAspectFit(path, scaleRatio, q=90, f='JPEG'):
+def resizeAspectFit(path, scaleRatio, q=90, f='JPEG', sampleFilter=Image.ANTIALIAS):
     dirs = os.listdir(path)
-    print(1)
     for imagePath in dirs:
         fullImagePath = os.path.join(path, imagePath)
-        print(fullImagePath)
         if os.path.isfile(fullImagePath):
-            print(3)
             if(isImageFile(fullImagePath)):
                 print("Scaling " + fullImagePath)
                 image = Image.open(fullImagePath)
                 name, ext = os.path.splitext(fullImagePath)
                 width, height = image.size
-                imResize = image.resize((int(width * scaleRatio), int(height * scaleRatio)), Image.ANTIALIAS)
+                imResize = image.resize((int(width * scaleRatio), int(height * scaleRatio)), sampleFilter)
                 imResize.save(name + ' resized.' + f, f, quality=q)
             else:
                 print("Skipping " + fullImagePath)
@@ -50,6 +47,8 @@ def parseArguments():
     parser.add_argument("-D", "--dir", type=str, help="Supply absolute directory path", required = False, default = "")
     parser.add_argument("-F", "--format", type=str, help="Saved Image format (JPEG, PNG)", required = False, default = "JPEG")
     parser.add_argument("-Q", "--quality", type=int, help="Saved Image quality (0-100)", required = False, default = 100)
+    parser.add_argument("-noAA", "--disable-antialias", dest='antialias', action='store_false')
+    parser.set_defaults(antialias=True)
     return parser.parse_args() 
 
 
@@ -59,15 +58,20 @@ def applyResizing(args):
     else:
         imagesDirPath = os.getcwd()
 
+    if(args.antialias):
+        sampleFilter = Image.ANTIALIAS
+    else:
+        sampleFilter = Image.NEAREST
+
     if(args.targetScaleRatio != 1.0):
         print("Resizing with target scale ratio " + str(args.targetScaleRatio))
-        resizeAspectFit(imagesDirPath, args.targetScaleRatio, args.quality, args.format)
+        resizeAspectFit(imagesDirPath, args.targetScaleRatio, args.quality, args.format, sampleFilter)
     else:
         if(args.targetHeight < 1 and args.targetWidth < 1):
             print("Please supply at least one value for width and/or height")
         else:
             print("Resizing with width " + str(args.targetWidth) + " and height " + str(args.targetHeight))
-            resize(imagesDirPath, args.targetWidth, args.targetHeight, args.quality, args.format)
+            resize(imagesDirPath, args.targetWidth, args.targetHeight, args.quality, args.format, sampleFilter)
     
 
 if __name__ == "__main__":
