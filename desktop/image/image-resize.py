78fa0eacc910c4fad7fc6f69aa10b89b150422c8
DESKTOP-CVQ6K9K\PC-LISBON <carotenuto.attilio@gmail.com>
2018-06-20 16:14:17 +0100

    image resize cmd util

---
+++ desktop/image/image-resize.py
@@ -0,0 +1,75 @@
+from PIL import Image
+import os, sys, argparse
+
+def resize(path, w, h, q=90, f='JPEG'):
+
+    dirs = os.listdir(path)
+
+    for imagePath in dirs:
+        fullImagePath = os.path.join(path, imagePath)
+        if os.path.isfile(fullImagePath):
+            if(isImageFile(fullImagePath)):
+                print("Resizing " + fullImagePath)
+                image = Image.open(fullImagePath)
+                name, ext = os.path.splitext(fullImagePath)
+                imResize = image.resize((w, h), Image.ANTIALIAS)
+                imResize.save(name + '_resized.' + f, f, quality=q)
+            else:
+                print("Skipping " + fullImagePath)
+
+
+def isImageFile(path):
+    lowerPath = path.lower()
+    return lowerPath.endswith(".jpg") or lowerPath.endswith(".png") or lowerPath.endswith(".gif") or lowerPath.endswith(".jpeg")
+
+
+def resizeAspectFit(path, scaleRatio, q=90, f='JPEG'):
+    dirs = os.listdir(path)
+    print(1)
+    for imagePath in dirs:
+        fullImagePath = os.path.join(path, imagePath)
+        print(fullImagePath)
+        if os.path.isfile(fullImagePath):
+            print(3)
+            if(isImageFile(fullImagePath)):
+                print("Scaling " + fullImagePath)
+                image = Image.open(fullImagePath)
+                name, ext = os.path.splitext(fullImagePath)
+                width, height = image.size
+                imResize = image.resize((int(width * scaleRatio), int(height * scaleRatio)), Image.ANTIALIAS)
+                imResize.save(name + ' resized.' + f, f, quality=q)
+            else:
+                print("Skipping " + fullImagePath)
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='Image Resizer Parameters')
+    parser.add_argument('-V', '--version', action='version', version='0.0.1')
+    parser.add_argument("-W", "--targetWidth", type=int, help="Saved Image width", required = False, default = -1)
+    parser.add_argument("-H", "--targetHeight", type=int, help="Saved Image height", required = False, default = -1)
+    parser.add_argument("-S", "--targetScaleRatio", type=float, help="Saved Image scale ratio", required = False, default = 1.0)
+    parser.add_argument("-D", "--dir", type=str, help="Supply absolute directory path", required = False, default = "")
+    parser.add_argument("-F", "--format", type=str, help="Saved Image format (JPEG, PNG)", required = False, default = "JPEG")
+    parser.add_argument("-Q", "--quality", type=int, help="Saved Image quality (0-100)", required = False, default = 100)
+    return parser.parse_args() 
+
+
+def applyResizing(args):
+    if args.dir:
+        imagesDirPath = args.dir
+    else:
+        imagesDirPath = os.getcwd()
+
+    if(args.targetScaleRatio != 1.0):
+        print("Resizing with target scale ratio " + str(args.targetScaleRatio))
+        resizeAspectFit(imagesDirPath, args.targetScaleRatio, args.quality, args.format)
+    else:
+        if(args.targetHeight < 1 and args.targetWidth < 1):
+            print("Please supply at least one value for width and/or height")
+        else:
+            print("Resizing with width " + str(args.targetWidth) + " and height " + str(args.targetHeight))
+            resize(imagesDirPath, args.targetWidth, args.targetHeight, args.quality, args.format)
+    
+
+if __name__ == "__main__":
+    args = parseArguments()
+    applyResizing(args)
