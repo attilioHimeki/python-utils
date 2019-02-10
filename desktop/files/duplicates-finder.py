153574f0cc8a40e3a73c4ce080e4a0417b2fd232
Attilio Carotenuto <carotenuto.attilio@gmail.com>
2019-02-11 00:46:01 +0100

    Allow files duplicate finder by filename rather than hash

--- desktop/files/duplicates-finder.py
+++ desktop/files/duplicates-finder.py
@@ -14,43 +14,67 @@ def calculateFileHash(path):
 def isFileNonEmpty(path):
     return os.path.getsize(path) > 0
 
-def getDuplicatesInFolder(folder):
-    pathsByHash = {}
+def getDuplicatesInFolderByHash(folder):
+    filesDict = {}
     for dirName, subDirs, fileList in os.walk(folder):
-        print('Scanning folder ' + dirName)
+        print('Scanning folder ' + dirName + " - By hash")
         for filename in fileList:
             path = os.path.join(dirName, filename)
             if(isFileNonEmpty(path)):
                 fileHash = calculateFileHash(path)
-                if fileHash in pathsByHash:
-                    pathsByHash[fileHash].append(path)
+                if fileHash in filesDict:
+                    filesDict[fileHash].append(path)
                 else:
-                    pathsByHash[fileHash] = [path]
+                    filesDict[fileHash] = [path]
             else:
                 print("Skipping empty file " + path)
-    return pathsByHash
+    return filesDict
 
-def processResult(pathsByHash):
-    values = pathsByHash.values()
+def getDuplicatesInFolderByFilename(folder):
+    filesDict = {}
+    for dirName, subDirs, fileList in os.walk(folder):
+        print('Scanning folder ' + dirName + " - By filename")
+        for filename in fileList:
+            path = os.path.join(dirName, filename)
+            if(isFileNonEmpty(path)):
+                if filename in filesDict:
+                    filesDict[filename].append(path)
+                else:
+                    filesDict[filename] = [path]
+            else:
+                print("Skipping empty file " + path)
+    return filesDict
+
+def processResult(filesDict):
+    values = filesDict.values()
+    foundDuplicates = False
     for fileOccurs in values:
         if(len(fileOccurs) > 1):
             fileName= os.path.basename(fileOccurs[0])
+            foundDuplicates = True
             print("\nFound duplicate " + fileName + " at:")
             for f in fileOccurs:
                 print(f)
+    if foundDuplicates == False:
+        print("No duplicates found")
 
 def parseArguments():
     parser = argparse.ArgumentParser(description='Duplicates Finder Parameters')
     parser.add_argument('-V', '--version', action='version', version='0.0.1')
     parser.add_argument("-d", "--dir", help="Supplied directory to navigate", type=str, required = True)
+    parser.add_argument("-f", "--byfilename", help="Compare files by filename instead of hash", dest='byfilename', action='store_true')
+    parser.set_defaults(byfilename=False)
     return parser.parse_args() 
 
 def findDuplicates(args):
     if args.dir:
         folderPath = args.dir
         if os.path.exists(folderPath):
-            pathsByHash = getDuplicatesInFolder(folderPath)
-            processResult(pathsByHash)
+            if args.byfilename:
+                filesDict = getDuplicatesInFolderByFilename(folderPath)
+            else:
+                filesDict = getDuplicatesInFolderByHash(folderPath)
+            processResult(filesDict)
         else:
             print("The supplied directory does not exist")
     else:
